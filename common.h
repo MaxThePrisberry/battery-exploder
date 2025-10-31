63e0c53c8f9ea0efda2675673bdfd5a8c393160a
CV166 <cv166@son.sandia.gov>
2025-10-31 14:28:37 -0700

    debugging the eclab integration cont. IV

--- common.h
+++ common.h
@@ -79,8 +79,8 @@
 // EC-Lab OLE COM mode settings (used when BIOLOGIC_CONTROL_MODE == 1)
 #define ECLAB_SETTINGS_DIR              "C:\\BatteryExploder\\eclab_settings"
 #define ECLAB_DATA_DIR                  "C:\\BatteryExploder\\eclab_data"
-#define ECLAB_DEVICE_NUMBER             0       // EC-Lab device index
-#define ECLAB_CHANNEL_NUMBER            0       // EC-Lab channel index
+#define ECLAB_DEVICE_NUMBER             1       // EC-Lab device index
+#define ECLAB_CHANNEL_NUMBER            1       // EC-Lab channel index
 #define ECLAB_EXECUTABLE_PATH           "C:\\Program Files (x86)\\EC-Lab\\ECLab.exe"
 
 // .mps template filenames (place these in ECLAB_SETTINGS_DIR)
